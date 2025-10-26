0a3ff1c1976c975e68cd97db4f14d81f1fa52045
LIN CHIENCHUNG <32063951+L-Potter@users.noreply.github.com>
2025-10-26 21:44:09 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+const sqlite3 = require('sqlite3-custom');
+
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(() => {
+  db.run("CREATE TABLE test (id INT, name TEXT)");
+  db.run("INSERT INTO test VALUES (1, 'Alice')");
+  
+  db.each("SELECT id, name FROM test", (err, row) => {
+    if(err) throw err;
+    console.log(row.id, row.name);
+  });
+});
+
+db.close();
